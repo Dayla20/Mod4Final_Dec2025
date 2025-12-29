3b158ddfbd1a284497dcb70cab0642f0276d51d4
Dayla20 <shilly_riser@yahoo.com>
2025-12-28 20:59:22 -0600

    Go6

--- user.js
+++ user.js
@@ -1,18 +1,22 @@
 const postListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id")
 
-async function main() {
-    const id = localStorage.getItem("Id")
+async function onSearchChange(event) {
+    const id= event.target.value;
+    renderPosts (id)
+}
+
+async function renderPosts(id) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json()
-     console.log(postsData);
-
-postListEl.innerHTML = postsData.map((post) => 
-   `<div class="post-list">
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('') 
+}
+function postHTML(post) {
+return `<div class="post-list">
     <div class="post">
       <div class="post__title">${post.title}</div>
       <p class="post__body">${post.body}</p>
     </div>
-  </div>`)
-  .join("")
+  </div>`
 }
-    main();
+    renderPosts(id);
